eff720f35109e355599b3b627c26a9f66f68c42f
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-03-06 13:00:43 -0500

    Add <hr> to UI for contacts list.

--- src/app/ui/contacts/ContactList.tsx
+++ src/app/ui/contacts/ContactList.tsx
@@ -8,15 +8,14 @@ export function ContactList(): React.ReactNode {
 
   return (
     <div className="text-center">
-
-       { contacts.map((c: FormData) => (
-          <Link key={c.id} href={`/contacts/${c.id}`}>
-            <p className="mb-2">
-              {c.firstName}, {c.lastName}
-            </p>
-          </Link>
-        ))}
-
+      {contacts.map((c: FormData) => (
+        <Link key={c.id} href={`/contacts/${c.id}`}>
+          <p className="mb-2 font-semibold">
+            {c.firstName}, {c.lastName}
+          </p>
+          <hr className="border-t-2 border-gray-300 mx-auto w-1/8 mb-2" />
+        </Link>
+      ))}
     </div>
   );
 }
