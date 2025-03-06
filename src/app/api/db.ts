e0a9cd2c8486bc7fa9c2b8e7ed915ff0bcb083ab
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-03-05 20:17:23 -0500

    Remove db url.

--- src/app/api/db.ts
+++ src/app/api/db.ts
@@ -1,5 +1,5 @@
 import postgres from 'postgres'
 
-const sql = postgres('postgresql://contacts_owner:npg_fqJAnoSidv97@ep-restless-truth-a8h0kyos-pooler.eastus2.azure.neon.tech/contacts?sslmode=require')
+const sql = postgres()
 
 export default sql
